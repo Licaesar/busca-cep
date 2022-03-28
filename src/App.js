695240cd9fdaed05447768d3178148eecd1290f6
Licaesar <licaesar@pop-os.localdomain>
2022-03-28 18:06:51 -0400

    add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Teste</h1>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu cep..."
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+      </div>
+
+      <main>
+        <h2>CEP: 72309420</h2>
+
+        <span>Rua Teste</span>
+        <span>Complemento: Algum</span>
+        <span>Vila Cavalo</span>
+        <span>Porto Velho - RO</span>
+      </main>
+
     </div>
   );
 }
